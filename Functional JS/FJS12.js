ad9e5968b56bac4ea3952d20cba486b097c55cb1
Chen Yiwen <lumpychen@outlook.com>
2016-02-03 13:28:28 +0800

    spy

--- Functional JS/FJS12.js
+++ Functional JS/FJS12.js
@@ -3,7 +3,12 @@
  */
 function Spy(target, method) {
     // SOLUTION GOES HERE
-
+    target.count = 0;
+    var oldFunc = target[ method ];
+    target[method] = function() {
+        target.count ++;
+        return oldFunc.apply(target, arguments);
+    }
+    return target
 }
-
 module.exports = Spy;
