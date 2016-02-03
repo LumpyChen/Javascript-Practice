ca1388105f977d8bc2e8a57826c9899074314445
Chen Yiwen <lumpychen@outlook.com>
2016-02-03 11:12:12 +0800

    reduce-map

--- Functional JS/FJS11.js
+++ Functional JS/FJS11.js
@@ -1,3 +1,14 @@
 /**
  * Created by Lumpychen on 16/2/3.
  */
+module.exports = function arrayMap(arr, fn) {
+    // SOLUTION GOES HERE
+    return arr.map(fn)
+}
+
+//module.exports = function arrayMap(arr, fn, thisArg) {
+//    return arr.reduce(function(acc, item, index, arr) {
+//        acc.push(fn.call(thisArg, item, index, arr))
+//        return acc
+//    }, [])
+//}
