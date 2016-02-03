832fa7a96a92f4652940f7794e6d777fa80fc515
Chen Yiwen <lumpychen@outlook.com>
2016-02-03 10:27:24 +0800

    apply

---
+++ Functional JS/FJS9.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Lumpychen on 16/2/3.
+ */
+var slice = Array.prototype.slice;
+
+function logger(namespace) {
+    // SOLUTION GOES HERE
+    return function (){
+        console.log(namespace,slice.apply(arguments).reduce(function (a,b) {
+            return a+' '+b;
+        }));
+    }
+}
+
+module.exports = logger
+
+
+//var slice = Array.prototype.slice
+//
+//function logger(namespace) {
+//    return function() {
+//        console.log.apply(console, [namespace].concat(slice.call(arguments)))
+//    }
+//}
+//
+//module.exports = logger
