b680f7cda5c6c338e70e4caa6c5edd78f4a406b3
Chen Yiwen <lumpychen@outlook.com>
2016-02-04 09:29:06 +0800

    unfinished

--- Functional JS/FJS17.js
+++ Functional JS/FJS17.js
@@ -1,14 +1,33 @@
 /**
  * Created by Lumpychen on 16/2/3.
  */
-function curryN(fn, n) {
-    // SOLUTION GOES HERE
-    var num = fn.length;
-    if (num<=0)
-    return;
-    else
-    --num;
-    return curryN(fn,n);
+function sub_curry(fn
+                   /*, variable number of args */
+) {
+    var args = [].slice.call(arguments, 1);
+    return function () {
+        return fn.apply(this, args.concat(Object.keys(arguments)));
+    };
 }
 
-module.exports = curryN
+function curryN(fn, length) {
+
+// capture fn's # of parameters
+    length = length || fn.length;
+    return function () {
+        if (arguments.length < length) {
+
+// not all arguments have been specified. Curry once more.
+            var combined = [fn].concat(Object.keys(arguments));
+            return length - arguments.length > 0
+                ? curryN(sub_curry.apply(this, combined), length - arguments.length)
+                : sub_curry.call(this, combined );
+        } else {
+
+// all arguments have been specified, actually call function
+            return fn.apply(this, arguments);
+        }
+    };
+}
+
+module.exports = curryN;
